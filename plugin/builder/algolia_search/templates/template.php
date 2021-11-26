0f7412487d4d9ce78b945ba4f3e9b2dafd55457f
Daniele Rosario <daniele@weble.it>
2021-11-26 09:32:36 +0100

    fixed search box issues

--- plugin/builder/algolia_search/templates/template.php
+++ plugin/builder/algolia_search/templates/template.php
@@ -12,11 +12,7 @@ $search = $this->el('ais-search-box', [
         'ais-SearchBox-form' => 'uk-form',
         'ais-SearchBox-input' => 'uk-input uk-form-small',
         'ais-SearchBox-reset' => 'uk-button uk-button-default uk-button-small uk-position-absolute uk-position-right',
-        'ais-SearchBox-submit' => 'uk-hidden', /*$this->expr([
-            'uk-hidden uk-width-1-1 {@fullwidth}',
-            'uk-{button_style: link-\w+}' => ['button_style' => $props['button_style']],
-            'uk-button uk-button-{!button_style: |link-\w+} [uk-button-{button_size}]' => ['button_style' => $props['button_style']],
-        ], $props)*/
+        'ais-SearchBox-submit' => 'uk-hidden',
     ])
 ])
 ?>
