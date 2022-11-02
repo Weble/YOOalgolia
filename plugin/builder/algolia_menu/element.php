0691208560d134e2cd7d0d25d2c9de743aa4ed51
Daniele Rosario <daniele@weble.it>
2022-11-02 12:24:08 +0100

    hierical menu

---
+++ plugin/builder/algolia_menu/element.php
@@ -0,0 +1,32 @@
+<?php
+
+
+return [
+
+    'transforms' => [
+
+
+        'render' => function ($node) {
+
+            $facets = [];
+            foreach ($node->props['facets'] as $facet) {
+
+                $facet = (array) $facet->props;
+                $facets[] = $facet['field'];
+            }
+
+            $node->facets = json_encode($facets);
+            $node->facets_count = count($facets);
+        }
+
+    ],
+
+    'fields' => [
+
+        'facet' => [
+            'type' => 'text',
+            'label' => 'Facet'
+        ]
+    ]
+
+];
