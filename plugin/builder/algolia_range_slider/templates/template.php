a99052820a07d58e4b62a769f93b1c0ffd3d7a99
Silvia <silvia@weble.it>
2022-11-23 10:15:41 +0100

    update range slider

--- plugin/builder/algolia_range_slider/templates/template.php
+++ plugin/builder/algolia_range_slider/templates/template.php
@@ -1,18 +1,24 @@
-<ais-range-input
-  attribute="<?php echo $props['attribute']; ?>"
-  :min="<?php echo $props['min']; ?>"
-  :max="<?php echo $props['max']; ?>"
-  :precision="<?php echo $props['precision']; ?>"
->
-  <template v-slot="{ currentRefinement, range, refine }">
+<?php
+$el = $this->el('div', []);
+?>
 
-    <vue-slider
-        :min="range.min"
-        :max="range.max"
-        :lazy="true"
-        :value="toValue(currentRefinement, range)"
-        @change="refine({ min: $event[0], max: $event[1] })"
-      />
-      
-  </template>
-</ais-range-input>
+<?= $el($props, $attrs); ?>
+    <ais-range-input
+            attribute="<?php echo $props['attribute']; ?>"
+            :min="<?php echo $props['min']; ?>"
+            :max="<?php echo $props['max']; ?>"
+            :precision="<?php echo $props['precision']; ?>"
+    >
+        <template v-slot="{ currentRefinement, range, refine }">
+
+            <vue-slider
+                    :min="range.min"
+                    :max="range.max"
+                    :lazy="true"
+                    :value="toValue(currentRefinement, range)"
+                    @change="refine({ min: $event[0], max: $event[1] })"
+            />
+
+        </template>
+    </ais-range-input>
+<?= $el->end(); ?>
